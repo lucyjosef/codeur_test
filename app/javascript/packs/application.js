e3fa0cef297a37ecb166d3c3accb5bbbec631791
lucyjosef <luciejoseph@ymail.com>
2020-05-27 17:46:51 +0200

    add css for ux + started vue modal

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,13 +2,30 @@
 
 import Vue from 'vue'
 import App from '../components/app.vue'
+// import './stylesheets/application.scss'
+import 'bootstrap'
+
+Vue.component('modal', {
+  template: '#modal-template',
+  props: ['show'],
+  methods: {
+    savePost: function () {
+      // Some save logic goes here...
+      
+      this.$emit('close');
+    }
+  }
+});
 
 document.addEventListener('DOMContentLoaded', () => {
   document.body.appendChild(document.createElement('app'))
   const app = new Vue({
     el: 'app',
     template: '<App/>',
-    components: { App }
+    components: { App },
+    data: {
+      showModal: false
+    }
   })
 
   console.log(app)
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,6 @@
 class Comment < ApplicationRecord
     belongs_to :event
+
+    validates :author, :presence => true
+    validates :message, :presence => true
 end
