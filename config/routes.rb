0211169720bdecd348ba0bfc4b77ee115995748b
lucyjosef <luciejoseph@ymail.com>
2020-05-27 13:28:39 +0200

    set events index as default view

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  
+
+  root "events#index"
+
   # do not use interpolation because of reserved word new
   resources :events, only: ["create", "index", "show", "new"] do
     resources :comments, only: %i[create index]
