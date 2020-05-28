04278e17f116f98e54e77ff6cbd8f9cb72f97750
lucyjosef <luciejoseph@ymail.com>
2020-05-28 09:45:49 +0200

    remove js import failed

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,32 +1,32 @@
 // app/javascript/packs/application.js
 
-import Vue from 'vue'
-import App from '../components/app.vue'
-// import './stylesheets/application.scss'
-import 'bootstrap'
+// import Vue from 'vue'
+// import App from '../components/app.vue'
+// // import './stylesheets/application.scss'
+// import 'bootstrap'
 
-Vue.component('modal', {
-  template: '#modal-template',
-  props: ['show'],
-  methods: {
-    savePost: function () {
-      // Some save logic goes here...
+// Vue.component('modal', {
+//   template: '#modal-template',
+//   props: ['show'],
+//   methods: {
+//     savePost: function () {
+//       // Some save logic goes here...
       
-      this.$emit('close');
-    }
-  }
-});
+//       this.$emit('close');
+//     }
+//   }
+// });
 
-document.addEventListener('DOMContentLoaded', () => {
-  document.body.appendChild(document.createElement('app'))
-  const app = new Vue({
-    el: 'app',
-    template: '<App/>',
-    components: { App },
-    data: {
-      showModal: false
-    }
-  })
+// document.addEventListener('DOMContentLoaded', () => {
+//   document.body.appendChild(document.createElement('app'))
+//   const app = new Vue({
+//     el: 'app',
+//     template: '<App/>',
+//     components: { App },
+//     data: {
+//       showModal: false
+//     }
+//   })
 
-  console.log(app)
-})
+//   console.log(app)
+// })
